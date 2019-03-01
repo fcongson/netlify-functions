6cc82b9b93cc6361f643c21d725244257d2492c6
Frank Congson <fcongson@gmail.com>
2019-03-02 12:45:30 +1300

    Add api call

---
+++ src/lambda/todo.js
@@ -0,0 +1,14 @@
+import * as axios from 'axios'
+
+exports.handler = (event, context, callback) => {
+  axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: res.data.title,
+      })
+    })
+    .catch((err) => {
+      callback(err)
+    })
+}
